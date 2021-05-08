fa7e8cbfb413784832bcb8fe1160e43a5fc1da15
hugo streppel <strh93@gmail.com>
2021-05-08 03:33:24 -0300

    Desafio react

---
+++ Desafio_ReactJS/src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: 'Learning REACT',
+      url: 'https://learnreact.com/',
+      techs: ['Node.js', 'ReactJS']
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
